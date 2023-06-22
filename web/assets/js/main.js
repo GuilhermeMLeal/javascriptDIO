d56108da7d748c902554f4f9c901e05be6e0500c
Guilherme Martins-global <guilherme.mleal.11@gmail.com>
2023-06-22 17:32:20 -0300

    funções arrow function

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,4 +1,14 @@
-const btnTeste = document.getElementById("btnTest")
-        btnTeste.addEventListener('click',function(){
-            console.log('Clicou no botão')
-        })
+
+const offset=0;
+const limit=10;
+
+const url= `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url)
+/*.then(function (response){
+    response.json().then(function(responseBody){
+        console.log(responseBody)
+    })
+})*/ // Não é legal ter uma resposta atrás de outras, mas sim chamar uma outra
+.then( (response)=> response.json).then((jsonBody)=>console.log(jsonBody))
+.catch( (error)=>console.error(error))
